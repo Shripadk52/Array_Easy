aeabb7a358f390136109eb66af86f18a241e8491
Shripad <kshripad469@gmail.com>
2025-08-30 07:41:20 +0530

    Adding the final Solution of the Contains Duplicate Problem

--- Contains_Duplicate/Contains_Duplicate.cpp
+++ Contains_Duplicate/Contains_Duplicate.cpp
@@ -2,19 +2,71 @@
 //
 
 #include <iostream>
+using namespace std;
+bool containsDuplicate(int* nums, int numsSize);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    /*Sample input and it's size*/
+    int nums[] = { 2,14,18,22,22 };
+    int numsSize = sizeof(nums) / sizeof(int);
+    /*Testing Sample on Function*/
+    bool result = containsDuplicate(nums, numsSize);
+    if (result == 1) {
+        printf("Contains Duplicate\n");
+    }
+    else {
+        printf("No Duplicate found\n");
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool containsDuplicate(int* nums, int numsSize) {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    if(numsSize <=1) /*If the array is empty or has only one element*/
+		return 0;   // Early return No duplicate found
+
+	bool DuplicateFound = 0;// Flag to check if duplicate is found
+	int* hashPtr = (int*)malloc(numsSize * sizeof(int));// pointer to hash table
+
+    /*Initialize the array with INT_MIN to avoid garbage values(aka Sentry)*/
+    for (int i = 0; i < numsSize; i++) {
+		hashPtr[i] = INT_MIN;
+    }
+	
+    for (int i = 0; i < numsSize; i++) {
+        /*Traverse through Array*/
+
+        int HashIndex = (((nums[i] % numsSize) + numsSize)% numsSize); /*Hash Function which 
+        give us Hash Index*/
+
+        if (hashPtr[HashIndex] == nums[i])/*If the Hash Index is already used for the same element*/
+        {
+            DuplicateFound = 1; /*Duplicate is found*/
+            free(hashPtr); /*Free the allocated memory*/
+            break; /*Break the loop since no further need to check*/
+        }
+        else if (hashPtr[HashIndex] != INT_MIN)
+        {/*To check if the Current Hash Index is used by another key to avoid collision*/
+            
+            // To avoid collision  we have to improve the logic to handle collisions
+			while(hashPtr[HashIndex] != INT_MIN){
+				HashIndex = (HashIndex + 1) % numsSize; // Circular incrementation to find empty available slot
+				//Keep incrementing the index until we find the sentinel value which indicates an empty slot
+
+                if (hashPtr[HashIndex] == nums[i])/*If the Hash Index is already used for the same element*/
+                {
+                    DuplicateFound = 1; /*Duplicate is found*/
+                    free(hashPtr); /*Free the allocated memory*/
+                    return DuplicateFound; /*Return the result of duplicate found or not*/
+                }
+            }
+			hashPtr[HashIndex] = nums[i]; /*Storing element in Hash table at empty location*/
+        }
+        else {
+            hashPtr[HashIndex] =  nums[i];/*Storing element in Hash table*/
+        }
+    }
+	return DuplicateFound; /*Return the result of duplicate found or not*/
+}
+
+
